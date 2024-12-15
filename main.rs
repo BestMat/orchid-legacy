89b93159f447ecbcacc6656ee1b29ae859328555
BestMat <tayoyuva@gmail.com>
2024-12-15 17:20:45 +0530

    Removed Types

--- main.rs
+++ main.rs
@@ -10,15 +10,6 @@ fn main() {
     println!("I Love You Amma");
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum Type {
-    N8(i8), N16(i16), N32(i32), N64(i64), N128(i128), // Numbers (Integers)
-    U8(u8), U16(u16), U32(u32), U64(u64), U128(u128), // Unsigned Numbers
-    F32(f32), F64(f64),                               // Floats
-    STR(String),                                      // Constant String
-    BOOL(bool),                                       // Boolean
-}
-
 // ---------------------------------------------------------------
 // |                                                             |
 // |******************* BESTORCHID - BESTMAT ********************|
