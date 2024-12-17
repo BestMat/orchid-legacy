51c949ee64573d1a37818b7ffb2e0d6faf544b9f
BestMat <tayoyuva@gmail.com>
2024-12-17 20:23:19 +0530

    Emptied Lexer

--- main.rs
+++ main.rs
@@ -1,12 +1,7 @@
 // ©2024 - BestOrchid - BestMat - Yuvanth.B - All rights reserved.
 
-use lexer::Lexer;
-
 #[path="src/lexer/lexer.rs"] mod lexer;
 
 fn main() {
     println!("I Love You Amma");
-    
-    let mut lexer = Lexer::new("(hello!123@ ");
-    lexer.print_tokens();
 }
--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,19 +1,10 @@
 // ©2024 - BestOrchid - BestMat - Yuvanth.B - All rights reserved.
-use std::str::Chars;
+use std::collections::{HashMap, VecDeque};
 
 #[derive(Debug)]
 pub struct Token {
     kind: TokenKind,
     value: &'static str,
-}
-
-#[derive(Debug)]
-pub struct Lexer <'x> {
-    chars: Chars<'x>,
-    chars_vec: Vec<char>,
-    ch: char,
-    cursor: usize,
-    len: usize,
 }
 
 #[derive(Debug)]
@@ -106,122 +97,4 @@ pub enum TokenKind {
     
     // EOF:
     EOF             // EOF
-}
-
-impl <'x> Lexer <'x> {
-    pub fn new(src: &'x str) -> Self {
-        return Self {
-            chars: src.chars(),
-            chars_vec: Vec::new(),
-            ch: '_',
-            cursor: 0,
-            len: 0,
-        };
-    }
-    
-    pub fn tokenize(&mut self) -> Vec<Token> {
-        let mut tokens: Vec<Token> = Vec::new();
-        
-        self.chars_vec = self.chars.clone().collect();
-        self.len = self.chars.clone().count();
-        
-        while let Some(token) = self.next() {
-            tokens.push(token);
-        }
-        
-        return tokens;
-    }
-    
-    pub fn next(&mut self) -> Option<Token> {
-        let mut next_char = self.eat();
-        
-        match next_char {
-            // Symbols:
-            Some('(') => Some(Token { kind: TokenKind::OpenParen, value: "(" }),
-            Some(')') => Some(Token { kind: TokenKind::CloseParen, value: ")" }),
-            Some('[') => Some(Token { kind: TokenKind::OpenBracket, value: "[" }),
-            Some(']') => Some(Token { kind: TokenKind::CloseBracket, value: "]" }),
-            Some('{') => Some(Token { kind: TokenKind::OpenBrace, value: "{" }),
-            Some('}') => Some(Token { kind: TokenKind::CloseBrace, value: "}" }),
-            Some(';') => Some(Token { kind: TokenKind::Semicolon, value: ";" }),
-            Some(':') => Some(Token { kind: TokenKind::Colon, value: ":" }),
-            Some(',') => Some(Token { kind: TokenKind::Comma, value: "," }),
-            Some('<') => {
-                if self.peek() == '=' {
-                    return Some(Token { kind: TokenKind::LessEquals, value: "<=" });
-                } else {
-                    return Some(Token { kind: TokenKind::LessOperator, value: "<" });
-                }
-            },
-            Some('>') => {
-                if self.peek() == '=' {
-                    return Some(Token { kind: TokenKind::GreaterEquals, value: "<=" });
-                } else {
-                    return Some(Token { kind: TokenKind::GreaterOperator, value: "<" });
-                }
-            },
-            Some('?') => Some(Token { kind: TokenKind::QuestionMark, value: "?" }),
-            Some('|') => Some(Token { kind: TokenKind::Pipe, value: "|" }),
-            Some('!') => Some(Token { kind: TokenKind::Bang, value: "!" }),
-            Some('\\') => Some(Token { kind: TokenKind::BackSlash, value: "\\" }),
-            Some('/') => {
-                if self.peek() == '/' {
-                    return Some(Token { kind: TokenKind::Comment, value: "//" });
-                } else {
-                return Some(Token { kind: TokenKind::Slash, value: "/" });
-                }
-            },
-            Some('&') => Some(Token { kind: TokenKind::Ampersand, value: "&" }),
-            Some('@') => Some(Token { kind: TokenKind::At, value: "@" }),
-            Some('~') => Some(Token { kind: TokenKind::Squiggly, value: "~" }),
-            Some('#') => Some(Token { kind: TokenKind::Hash, value: "#" }),
-            Some('$') => Some(Token { kind: TokenKind::Dollar, value: "$" }),
-            Some('\'') => Some(Token { kind: TokenKind::Quote, value: "'" }),
-            Some('"') => Some(Token { kind: TokenKind::DoubleQuote, value: "\"" }),
-            Some('=') => {
-                if self.peek() == '=' {
-                    return Some(Token { kind: TokenKind::Equals, value: "==" });
-                } else if self.peek() == '>' {
-                    return Some(Token { kind: TokenKind::FatArrow, value: "=>" });
-                } else {
-                    return Some(Token { kind: TokenKind::Assignment, value: "=" });
-                }
-            },
-            Some('-') => {
-                if self.peek() == '>' {
-                    return Some(Token { kind: TokenKind::ThinArrow, value: "->" });
-                } else {
-                    return Some(Token { kind: TokenKind::Minus, value: "-" });
-                }
-            },
-            
-            // Other Arithmetic: (+, *, %, ^)
-            Some('+') => Some(Token { kind: TokenKind::Plus, value: "+" }),
-            Some('*') => Some(Token { kind: TokenKind::Star, value: "*" }),
-            Some('%') => Some(Token { kind: TokenKind::Percent, value: "%" }),
-            Some('^') => Some(Token { kind: TokenKind::Exponent, value: "^" }),
-            Some('a'..='z' | 'A' ..= 'Z' | '_') => {
-                todo!();
-            }
-            _ => {
-                self.cursor -= 1;
-                self.ch = self.chars.next_back()?;
-                return None;
-            }
-        }
-    }
-    
-    pub fn peek(&self) -> char {
-        return self.chars.clone().next().unwrap();
-    }
-    
-    pub fn eat(&mut self) -> Option<char> {
-        self.cursor += 1;
-        self.ch = self.chars.next()?;
-        return Some(self.ch);
-    }
-    
-    pub fn print_tokens(&mut self) {
-        println!("{:#?}", self.tokenize());
-    }
 }
